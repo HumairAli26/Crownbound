756914b10e02a5efe1bff65214c5d8d083d86f98
HumairAli26 <humairali2612@gmail.com>
2026-04-28 23:00:22 +0500

    Loaded Components in Shadow Wolf's Script

--- Assets/Crownbound/Scripts/Shadow_Wolf.cs
+++ Assets/Crownbound/Scripts/Shadow_Wolf.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class Shadow_Wolf : MonoBehaviour
 {
-    
+    public float speed = 4f;
+    private Animator am;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        am = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
