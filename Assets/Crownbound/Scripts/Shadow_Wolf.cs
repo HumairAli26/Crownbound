de9a71e8c6e6df8ee4cfbc9c0c57f4b47bebfb82
HumairAli26 <humairali2612@gmail.com>
2026-04-29 22:19:24 +0500

    Added movement code to shadow wolf's script
    Made it so that it follows the player at all times after spawning and then attacks it when it gets close

--- Assets/Crownbound/Scripts/Shadow_Wolf.cs
+++ Assets/Crownbound/Scripts/Shadow_Wolf.cs
@@ -1,22 +1,47 @@
 using UnityEngine;
 
-public class Shadow_Wolf : MonoBehaviour
+public class ShadowWolf : MonoBehaviour
 {
-    public float speed = 4f;
-    private Animator am;
+    public Transform player;
+    public float moveSpeed = 3f;
+    public float attackDistance = 1.5f;
     private Rigidbody2D rb;
-    private SpriteRenderer sr;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator anim;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        am = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        // Automatically find player
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player == null)
+            return;
+
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        // Move toward player
+        if (distance > attackDistance)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = new Vector2(direction.x * moveSpeed, rb.linearVelocity.y);
+            anim.SetBool("Run", true);
+
+            // Flip enemy
+            if (direction.x > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else if (direction.x < 0)
+                transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            // Stop and attack
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+            anim.SetBool("Run", false);
+            anim.SetTrigger("Attack");
+        }
     }
 }
