00060140bcfc0a66f53a44210e3d69aa2a956e7e
HumairAli26 <humairali2612@gmail.com>
2026-05-15 18:03:27 +0500

    Added movement code for spirit

--- Assets/Crownbound/Scripts/Spirit.cs
+++ Assets/Crownbound/Scripts/Spirit.cs
@@ -2,46 +2,63 @@ using UnityEngine;
 
 public class Spirit : MonoBehaviour
 {
-    public Transform player;
-
     public float moveSpeed = 3f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveDistance = 5f;
+    private Vector3 startPosition;
+    private bool movingRight = true;
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 
-    void MoveTowardPlayer()
+    void Move()
     {
-        float direction = Mathf.Sign(player.position.x - transform.position.x);
-        rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
-    }
-
-    void FlipTowardPlayer()
-    {
-        if (player.position.x > transform.position.x)
+        if (movingRight)
         {
-            transform.localScale =
-                new Vector3(
-                    originalScale.x,
-                    originalScale.y,
-                    originalScale.z
-                );
+            transform.position +=
+                Vector3.right *
+                moveSpeed *
+                Time.deltaTime;
+
+            // Reached right limit
+            if (transform.position.x >=
+                startPosition.x + moveDistance)
+            {
+                movingRight = false;
+
+                Flip();
+            }
         }
         else
         {
-            transform.localScale =
-                new Vector3(
-                    -originalScale.x,
-                    originalScale.y,
-                    originalScale.z
-                );
+            transform.position +=
+                Vector3.left *
+                moveSpeed *
+                Time.deltaTime;
+
+            // Reached left limit
+            if (transform.position.x <=
+                startPosition.x - moveDistance)
+            {
+                movingRight = true;
+
+                Flip();
+            }
         }
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+
+        scale.x *= -1;
+
+        transform.localScale = scale;
     }
 }
