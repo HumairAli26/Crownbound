757bb3f1815f0ffb732c0cf51c978150be1f4774
HumairAli26 <humairali2612@gmail.com>
2026-05-17 15:49:24 +0500

    Wrote spiritshots code so that it moves towards player when launched

--- Assets/Crownbound/Scripts/Spirit_Shot.cs
+++ Assets/Crownbound/Scripts/Spirit_Shot.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class Spirit_Shot : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float damage = 20f;
+
+    public float speed = 10f;
+
+    private Vector2 moveDirection;
+
+    // Called when projectile is spawned
+    public void SetTarget(Vector2 targetPosition)
     {
-        
+        // Calculate direction toward target
+        moveDirection = (targetPosition - (Vector2)transform.position).normalized;
+        // Flip sprite
+        if (moveDirection.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)(moveDirection * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Damage player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth health = collision.gameObject.GetComponent<PlayerHealth>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+        Destroy(gameObject);
     }
 }
